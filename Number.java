a13bfb67967b8d6c5d4505a4ce91f605ceeb8dbf
bryancheng66 <bryancheng66@gmail.com>
2020-11-28 21:34:09 -0500

    add equals(RationalNumber) method to rationalNumber

--- Number.java
+++ Number.java
@@ -4,12 +4,20 @@ public abstract class Number{
 	public boolean equals(RealNumber other){
 		if (getValue() == 0){
 			return other.getValue() == 0;
+		} else if (other.getValue() == 0) {
+			return getValue() == 0;
 		} else {
 			return Math.round(getValue() * 100000) == Math.round(other.getValue() * 100000);
 		}
 	}
 
 	public int compareTo(Number other){
-		return getValue() - other.getValue();	
+		if (getValue() == other.getValue()){
+			return 0;
+		} else if (getValue() < other.getValue()){
+			return -1;
+		} else {
+			return 1;
+		}
 	}
 }
