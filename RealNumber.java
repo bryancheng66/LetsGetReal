307121f1ee139440c3814dc849254d6b67163a72
bryancheng66 <bryancheng66@gmail.com>
2020-11-28 20:28:38 -0500

    delete redundant methods

--- RealNumber.java
+++ RealNumber.java
@@ -13,14 +13,6 @@ public class RealNumber extends Number {
 		return ""+getValue();
 	}
 
-	public boolean equals(RealNumber other){
-		if (getValue() == 0){
-			return other.getValue() == 0;
-		} else {
-			return Math.round(getValue() * 100000) == Math.round(other.getValue() * 100000);
-		}
-	}
-
 	public RealNumber add(RealNumber other){
 		return new RealNumber(getValue() + other.getValue());
 	}
